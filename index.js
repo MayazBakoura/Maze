49d693471673f0c3b503f08eea9b07fea6aca00f
MayazBakoura <mayazbakoura@gmail.com>
2026-03-06 01:14:04 +0400

    feat: Added maze structure generator that changes with user input cells

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const {Engine ,Render, Runner,World,Bodies,MouseConstraint,Mouse} = Matter;
+const {Engine ,Render, Runner,World,Bodies} = Matter;
 
 
-
-const  width=800;
+const cells=6;
+const  width=600;
 const  height=600;
+
+
+const unitLength = width /cells;
+
+
+
 const engine = Engine.create();
 const  {world} = engine; //world is created when creating engine
 const render = Render.create({
 element:document.body,
 engine:engine,
 options:{
-    wireframes:false,
+    wireframes:true,
     width,
     height
 }
@@ -21,32 +27,136 @@ options:{
 Render.run(render);
 Runner.run(Runner.create(),engine);
 
-World.add(world,MouseConstraint.create(engine,{
-    mouse:Mouse.create(render.canvas)
-    
-}));
-
 
 
 //wall
 const walls = [
-Bodies.rectangle(400,0,800,40,{isStatic:true}),
-Bodies.rectangle(400,600,800,40,{isStatic:true}),
-Bodies.rectangle(0,300,40,600,{isStatic:true}),
-Bodies.rectangle(800,300,40,600,{isStatic:true})  ];
+Bodies.rectangle(width/2,0,width,40,{isStatic:true}),
+Bodies.rectangle(width/2,height,width,40,{isStatic:true}),
+Bodies.rectangle(0,height/2,40,height,{isStatic:true}),
+Bodies.rectangle(width,height/2,40,height,{isStatic:true})  ];
 
 World.add(world,walls);
 
-for(let i=0;i<=20;i++){
-if(Math.random() >0.5){
 
-World.add(world,Bodies.rectangle(Math.random()*width,Math.random()*height,30,30));
+
+const shuffle=(arr) =>{
+    let counter = arr.length;
+
+
+    while(counter > 0){
+const index= Math.floor(Math.random() * counter);
+counter--;
+
+const temp = arr[counter];
+arr[counter] =  arr[index];
+arr[index]=temp;
+
+    }
+    return arr;
+};
+
+
+
+
+const  grid =Array(cells).fill(null).map(()=>Array(cells).fill(false));
+
+const verticals = Array(cells).fill(null).map(()=>{return Array(cells-1).fill(false)});
+const horizontals = Array(cells-1).fill(null).map(()=>{ return Array(cells).fill(false)});
+
+const startRow = Math.floor(Math.random() *  cells );
+const startColumn = Math.floor(Math.random() *  cells )
+
+const  stepThroughCell = (row,column) =>{
+//if i have visited the cell at [row,colum] ,then return
+if(grid[row][column])
+    return;
+//Mark this cell as being visited
+grid[row][column] =true;
+//Assemble randomly-ordered list of neighbors
+const neighbors = shuffle( [
+[row-1,column,'up'],
+[row,column+1,'right'],
+[row+1,column,'down'],
+[row,column-1,'left']
+
+]);
+
+//For each neighbor...
+for(let neighbor of neighbors){
+
+    const [nextRow,nextColumn ,direction] = neighbor;
+
+//See if neighbor is out of bounds
+if(nextRow < 0 || nextRow >= cells || nextColumn >= cells || nextColumn < 0)
+    continue;
+
+//if we visited that neighbor , continue to next neighbor
+if(grid[nextRow][nextColumn])
+    continue;
+//remove wall from horiz or verticals
+if(direction ==='left'){
+    verticals[row][column-1] = true;
+}else if(direction ==='right'){
+      verticals[row][column] = true;
+}else if(direction ==='up'){
+ horizontals[row-1][column] =true;
+}else if(direction ==='down'){
+    horizontals[row][column] = true;
 }
-else{
-    World.add(world,Bodies.circle(Math.random()*width,Math.random()*height,35,{
-        render:{
-            fillStyle:'darkred'
-        }
-    }));
+
+//visit the next cell
+stepThroughCell(nextRow,nextColumn);
 }
+
+};
+
+
+stepThroughCell(startRow,startColumn);
+
+horizontals.forEach((row ,rowIndex) =>{
+    row.forEach((open, columnIndex) => {
+if(open)
+    return;
+const wall= Bodies.rectangle(
+columnIndex * unitLength +unitLength/2,
+rowIndex * unitLength + unitLength,
+unitLength,
+10,
+{
+    isStatic: true
 }
+);
+World.add(world,wall);
+});
+
+
+verticals.forEach((row,rowIndex) =>{
+
+   row.forEach((open, columnIndex) => {
+if(open)
+    return;
+const wall= Bodies.rectangle(
+columnIndex * unitLength +unitLength,
+rowIndex * unitLength + unitLength/2,
+10,
+unitLength,
+
+{
+    isStatic: true
+}
+);
+World.add(world,wall);
+});
+
+
+
+
+
+});
+
+
+
+
+
+});
